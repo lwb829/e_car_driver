6f402d69012a6aa237d6d0990739447d74c8c354
lwb829 <846292774@qq.com>
2024-01-16 19:27:23 +0800

    20240116

--- build/catkin_generated/installspace/e_car_msg-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${e_car_msg_DIR}/.." "msg" e_car_msg_MSG_INCLUDE_DIRS UNIQUE)
-set(e_car_msg_MSG_DEPENDENCIES std_msgs)
--- build/devel/share/e_car_msg/cmake/e_car_msg-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(e_car_msg_MSG_INCLUDE_DIRS "/media/lwb829/本地磁盘/无人车项目/github仓库/e_car_driver/e_car_driver_msgs/msg")
-set(e_car_msg_MSG_DEPENDENCIES std_msgs)
